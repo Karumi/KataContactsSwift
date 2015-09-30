023c0d38e087425bdcb689fec5a998848062028c
davideme <davide@gokarumi.com>
2015-09-30 15:20:39 +0200

    Initial Commit

---
+++ ContactsApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ContactsApp
+//
+//  Created by Davide Mendolia on 30/09/15.
+//  Copyright © 2015 GoKarumi S.L. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
