6480d06b9fb359c2b99bad18aeee01549425ccd4
Rohit Bansal <rohitbansal.hcst@gmail.com>
2015-10-04 12:19:51 +0530

    Moved function methods into initializer.

--- ContactsApp/ContactsInitializer.swift
+++ ContactsApp/ContactsInitializer.swift
@@ -26,4 +26,56 @@ class ContactsInitializer {
         }
     }
     
+    // MARK: UI Inputs For Contact Details
+    
+    func getFirstName() -> String {
+        print("Firstame: ", terminator: "")
+        return readLine() ?? ""
+    }
+    func getLastName() -> String {
+        print("Lastname: ", terminator: "")
+        return readLine() ?? ""
+    }
+    func getPhone() -> String {
+        print("Phonenumber: ", terminator: "")
+        return readLine() ?? ""
+    }
+    
+    // MARK: Operations on contact
+    
+    func listContacts() {
+        print("Listing contacts ...")
+        let getContacts = GetContacts(contactRepository: contactRepository)
+        let contacts = getContacts.execute()
+        showContacts(contacts)
+    }
+    
+    func addContact() {
+        let firstName = getFirstName()
+        let lastName = getLastName()
+        let phone = getPhone()
+        let contact = Contact(firstName: firstName, lastName: lastName, phonenumber : phone)
+        if(isContactInfoValid(contact)) {
+            saveContactInRepo(contact)
+        } else {
+            print("Invalid contact details.")
+        }
+    }
+    
+    func saveContactInRepo(contact: Contact) {
+        let addContact = AddContact(contactRepository: contactRepository)
+        addContact.execute(contact)
+    }
+    
+    func showContacts(contacts: [Contact]) {
+        if contacts.count > 0 {
+            contacts.forEach {
+                print($0)
+            }
+            print("Total: \(contacts.count)")
+        } else {
+            print("No contacts")
+        }
+    }
+    
 }
